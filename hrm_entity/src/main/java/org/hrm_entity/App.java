a8a67f3dde5ada39f9117185dfe178203e8da311
zhongjun96 <zjdwrzh@live.cn>
2017-01-04 10:02:22 +0800

    BOS物流系统
    搭建基础

---
+++ hrm_entity/src/main/java/org/hrm_entity/App.java
@@ -0,0 +1,13 @@
+package org.hrm_entity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
